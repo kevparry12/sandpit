415afe1c08faa8f202430ea5229dc769b842b844
Kev <kevparry@btopenworld.com>
2018-01-07 18:57:01 +0000

    Added a second script to test functionality.

---
+++ secondtest.R
@@ -0,0 +1,11 @@
+# An R script to test out
+
+# Define vector of random variables that are normally distributed
+# with mean = 0 and sd = 1
+
+x <- rnorm(5000,0,1)
+
+# Plot the vector of variables as a histogram
+plot(x)
+
+# script ends...
